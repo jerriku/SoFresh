cb2ec765564bc671b57f849cd3cba30c715f822b
Jeric Fajardo <jeric.fajardo@playtech.com>
2022-06-15 21:17:03 +0100

    Add media icons for homepage and add homepage and navbar styling

---
+++ client/src/components/main/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <ul className="nav-bar">
+            <li className="nav-item">Home</li>
+            <li className="nav-item">Search</li>
+            <li className="nav-item">Planner</li>
+            <li className="nav-item">Account</li>
+        </ul>
+    );
+}
--- client/src/components/main/homepage/Home.jsx
+++ client/src/components/main/homepage/Home.jsx
@@ -1,7 +1,38 @@
+import Navbar from "../Navbar";
+import '../style.css';
+
 export default function Home() {
+    const firstFruit  = require("../../../media/fruits-1.svg");
+    const secondFruit = require("../../../media/fruits-2.svg");
+    const thirdFruit  = require("../../../media/fruits-3.svg");
+    const fourthFruit = require("../../../media/fruits-4.svg");
+
     return (
         <>
-            <h1>SoFresh</h1>
+            <header className="home-header">
+                <h1>SoFresh</h1>
+            </header>
+            <main>
+                <ul className="pro-list">
+                    <li className="pro-item">
+                        <img src={firstFruit} alt="A pear and an apple."/>
+                        <p>100% organic</p>
+                    </li>
+                    <li className="pro-item">
+                        <img src={secondFruit} alt="An orange."/>
+                        <p>healthy cooking</p>
+                    </li>
+                    <li className="pro-item">
+                        <img src={thirdFruit} alt="A kiwi."/>
+                        <p>always fresh</p>
+                    </li>
+                    <li className="pro-item">
+                        <img src={fourthFruit} alt="A grape."/>
+                        <p>best quality</p>
+                    </li>
+                </ul>
+            </main>
+            <Navbar />
         </>
     );
 }
